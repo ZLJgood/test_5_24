b0d401fe7b558ed41c9f8af9a57c2cbd3bde8885
麟杰 周 <2698283008@qq.com>
2022-05-23 23:50:25 +0800

    添加项目文件。

---
+++ test.c
@@ -0,0 +1,406 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int i = 0;
+//	double arr[10] = { 0 };
+//	double sum = 0;
+//	for (i = 0; i < 9; i++)
+//	{
+//		scanf("%lf", &arr[i]);
+//	}
+//	for (i = 0; i < 8; i++)
+//	{		
+//		int ave = 0;		
+//		ave=1.0* ((arr[i] + arr[i+1]) / 2);
+//		sum += sqrt(ave);
+//	}
+//	
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[10000];
+//	gets(arr);
+//	if (arr == 'a')
+//	{
+//		int a = 0;
+//		a++;		
+//	}
+//	if (arr == 'b')
+//	{
+//		int b = 0;
+//		b++;
+//	}
+//	if (arr == 'c')
+//	{
+//		int c = 0;
+//		c++;
+//	}
+//	if (arr == 'd')
+//	{
+//		int d = 0;
+//		d++;
+//	}
+//	if (arr == 'e')
+//	{
+//		int e = 0;
+//		e++;
+//	}
+//	if (arr == 'f')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'g')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'h')
+//	{
+//		int a = 0;
+//		a++;
+//	}if (arr == 'i')
+//	{
+//		int a = 0;
+//		a++;
+//	}if (arr == 'j')
+//	{
+//		int a = 0;
+//		a++;
+//	}if (arr == 'k')
+//	{
+//		int a = 0;
+//		a++;
+//	}if (arr == 'l')
+//	{
+//		int a = 0;
+//		a++;
+//	}if (arr == 'm')
+//	{
+//		int a = 0;
+//		a++;
+//	}if (arr == 'n')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'o')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'p')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'q')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'r')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 's')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 't')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'u')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'v')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'w')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'x')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'y')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	if (arr == 'z')
+//	{
+//		int a = 0;
+//		a++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[10000];
+//	scanf("%s", &arr);
+//	int st = 0;
+//	int a=0, b=0, c=0, d=0, e=0, f=0, g=0, h=0, i=0, j=0, k=0, l=0, m=0, n=0, o=0, p=0, q=0, r=0, s=0, t=0, u=0, v=0, w=0, x=0, y=0, z = 0;
+//	while (arr >= 'a' && arr <= 'z')
+//	{
+//		for (st = 97; st <= 122; st++)
+//		{
+//				if (arr == 'a')
+//	            {
+//		          a++;		
+//	            }
+//	             if (arr == 'b')
+//	           {
+//
+//		          b++;
+//	             }
+//	if (arr == 'c')
+//	{
+//		
+//		c++;
+//	}
+//	if (arr == 'd')
+//	{
+//		
+//		d++;
+//	}
+//	if (arr == 'e')
+//	{
+//		
+//		e++;
+//	}
+//	if (arr == 'f')
+//	{
+//		
+//		f++;
+//	}
+//	if (arr == 'g')
+//	{
+//		
+//		g++;
+//	}
+//	if (arr == 'h')
+//	{
+//		
+//		h++;
+//	}
+//	if (arr == 'i')
+//	{
+//
+//		i++;
+//	}
+//	
+//		if (arr == 'j')
+//	{
+//		
+//		j++;
+//	}
+//	if (arr == 'k')
+//	{
+//		
+//		k++;
+//	}
+//	if (arr == 'l')
+//	{
+//		
+//		l++;
+//	}
+//	if (arr == 'm')
+//	{
+//		
+//		m++;
+//	}
+//	if (arr == 'n')
+//	{
+//		
+//		n++;
+//	}
+//	if (arr == 'o')
+//	{
+//		
+//		o++;
+//	}
+//	if (arr == 'p')
+//	{
+//		
+//		p++;
+//	}
+//	if (arr == 'q')
+//	{
+//		q++;
+//	}
+//	if (arr == 'r')
+//	{
+//		r++;
+//	}
+//	if (arr == 's')
+//	{
+//		
+//		s++;
+//	}
+//	if (arr == 't')
+//	{
+//		
+//		t++;
+//	}
+//	if (arr == 'u')
+//	{
+//		
+//		u++;
+//	}
+//	if (arr == 'v')
+//	{
+//		
+//		v++;
+//	}
+//	if (arr == 'w')
+//	{
+//		
+//		w++;
+//	}
+//	if (arr == 'x')
+//	{
+//		
+//		x++;
+//	}
+//	if (arr == 'y')
+//	{
+//	
+//		y++;
+//	}
+//	if (arr == 'z')
+//	{
+//		
+//		z++;
+//	}
+//		}
+//	}
+//	printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
+//	return 0;
+//}
+
+
+//
+//#include<string.h>
+//#define N 100 
+//main()
+//{
+//	int i, j = 0, pp[26] = { 0 };
+//	char c[N];
+//	printf("请输入一个字符串：");
+//	gets(c);
+//	for (i = 0; i < strlen(c); i++)/*外循环扫描/遍历字符数组*/
+//		for (j = 0; j < 26; j++)
+//			if (c[i] == 'a' + j)
+//			{
+//				pp[j]++;
+//				break;
+//			}
+//	for (i = 0; i < 26; i++)
+//		printf("%c:%d  ", 'a' + i, pp[i]);//输出字母a-z及其出现的次数
+//
+//}
+//请编写程序：统计一串字符串中'a' 到 'z' 26个小写字母各自出现的次数，并依次放在pp所指数组中。 
+
+
+//int main()
+//{
+//	int b = 0;
+//	int a = 0;
+//	int i = 0;
+//	int arr[] = { 9,8,5,6,7,4,3,2,1 };	
+//	int pp[9];
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	/*for (i = 0; i < sz; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}*/
+//	
+//	for (i = 0; i < sz; )
+//	{
+//		int min = arr[0];
+//		int max = arr[0];
+//		
+//		for ( a = i; a < sz; a++)
+//		{
+//			if (arr[a] < min)
+//			{
+//				min = arr[a];
+//			}
+//		}
+//		arr[i] = min;
+//		i++;
+//		
+//		for ( b = i; b < sz; b++);
+//		{
+//			
+//			if (arr[b] > max)
+//			{
+//				max = arr[b];
+//			}
+//		}
+//		arr[i] = max;
+//		i++;
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int arr[] = { 9,5,8,7,6,4,1,2,3 };
+	int i, j;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+	int left = 0;
+	int right = n - 1;
+	int pp[9] = { 0 };
+	int t = 0;
+	while (left <= right)
+	{
+		pp[t] = arr[left];
+		t++;
+		pp[t] = arr[right];
+		t++;
+		left++;
+		right--;
+	}
+	for (i = 0; i < 9; i++)
+	{
+		printf("%d ", pp[i]);
+	}
+
+	return 0;
+}
